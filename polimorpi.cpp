cddbbb9c254361015b43623f1fc8f83b180cb477
Viaasyafrina16 <syafrinavia@gmail.com>
2024-05-28 15:09:35 +0700

    membuat deklarasi fungsi

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -4,5 +4,16 @@ using namespace std;
 class seseorang {
 public :
     virtual void pesan() = 0;
-    //
+    //virtual void pesan(){
+    //cout << "pesan dari seseorang"<< endl;
+    //}
+};
+
+class joko : public seseorang {
+public :
+    void pesan() {
+        
+    }
+
+}
 }
