e606079ddeca8b2915578ecb6b908fabd059e1f4
Viaasyafrina16 <syafrinavia@gmail.com>
2024-05-28 14:59:17 +0700

    membuat fungsi utama

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -35,7 +35,16 @@ public:
 class budi : public pekerja,public pelajar {
 public:
 
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur)
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
 
+int main() {
+    
 }
 
 
